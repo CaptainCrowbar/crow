f204cd7c6e25150701272f4788a86470e9821c81
Ross Smith <rosssmith@me.com>
2022-10-23 01:01:06 +1100

    Extract all required properties

--- source/crow/ucd/ucd.hpp
+++ source/crow/ucd/ucd.hpp
@@ -39,21 +39,31 @@ namespace Crow {
         Zs   // Space_Separator
     )
 
+    CROW_ENUM_CLASS(East_Asian_Width, int, 1,
+        Ambiguous,
+        Fullwidth,
+        Halfwidth,
+        Narrow,
+        Neutral,
+        Wide
+    )
+
     namespace Detail {
 
         using GeneralCategoryTable                = std::map<char32_t, GC>;
         using CanonicalCombiningClassTable        = std::map<char32_t, int>;
         using CanonicalDecompositionMappingTable  = std::unorderedMap<char32_t, std::vector<char32_t>>;
+        using EastAsianWidthTable                 = std::map<char32_t, East_Asian_Width>;
         using BooleanPropertyTable                = std::map<char32_t, bool>;
 
         const GeneralCategoryTable&                general_category_table();
         const CanonicalCombiningClassTable&        canonical_combining_class_table();
         const CanonicalDecompositionMappingTable&  canonical_decomposition_mapping_table();
-        const BooleanPropertyTable&                xid_continue_table();
-        const BooleanPropertyTable&                xid_start_table();
-        const BooleanPropertyTable&                xid_nonstart_table();
+        const EastAsianWidthTable&                 east_asian_width_table();
         const BooleanPropertyTable&                full_composition_exclusion_table();
         const BooleanPropertyTable&                pattern_syntax_table();
+        const BooleanPropertyTable&                xid_continue_table();
+        const BooleanPropertyTable&                xid_start_table();
 
     }
 
